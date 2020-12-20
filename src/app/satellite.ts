7c8a1ccc3da3d2e4e0b8e025d8e8881b248394d6
jilo1 <jilo1@DESKTOP-45N3BGQ>
2020-12-19 19:00:53 -0600

    second commit

---
+++ src/app/satellite.ts
@@ -0,0 +1,19 @@
+// import { Satellite } from './satellite';
+export class Satellite {
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+    this.name=name;
+    this.type=type;
+    this.orbitType=orbitType;
+    this.launchDate=launchDate;
+    this.operational=operational;  
+}
+shouldShowWarning():boolean{
+    return this.type==='Space Debris';
+   }
+
+}
